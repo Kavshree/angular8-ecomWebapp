5fb45f7478a2975965b8d8b7b31b7603572442d7
Kavshree <kavyashri.bj@gmail.com>
2020-06-13 19:12:38 +0530

    captured id from paramMap

--- src/app/Utilities/reuseable/ecom.sharedModule.module.ts
+++ src/app/Utilities/reuseable/ecom.sharedModule.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from "@angular/common";
+import { HttpClientModule }    from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ProdListViewTemplateComponent } from './prodListView.reuseable.component';
 import {ProdSingleItemViewTemplateComponent} from './prodSingleItemView.component.'
 import {BreadcrumbComponent} from './ecomapp.breadcrumb';
+import { ProdClothesService } from '../../Utilities/services/ecomapp.clothes.Service'
+
 @NgModule({
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,HttpClientModule,RouterModule],
   declarations: [ProdListViewTemplateComponent, ProdSingleItemViewTemplateComponent,BreadcrumbComponent],
-  providers: [],
+  providers: [ProdClothesService],
   exports: [ProdListViewTemplateComponent,ProdSingleItemViewTemplateComponent,BreadcrumbComponent]
 })
 export class SharedModule { }
--- src/app/Utilities/reuseable/prodSingleItemView.component..ts
+++ src/app/Utilities/reuseable/prodSingleItemView.component..ts
@@ -1,11 +1,26 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ProdClothesService } from '../../Utilities/services/ecomapp.clothes.Service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'prod-item',
   template: `<div>{{prodId}}</div>`,
-  styleUrls: ['../common/css/ecomStyles.css']
+  styleUrls: ['../common/css/ecomStyles.css'],
+  providers: [ProdClothesService]
 })
 
 export class ProdSingleItemViewTemplateComponent {
-    @Input("prodListArr") prodId = 0;
+    prodId : string;
+    sub;
+    constructor(protected _activatedRoute: ActivatedRoute) {}
+
+    ngOnInit() {
+      this.sub = this._activatedRoute.params.subscribe(params => {
+         this.prodId = params['id'];
+        
+         // In a real app: dispatch action to load the details here.
+      });
+    }
+    
+
    }
